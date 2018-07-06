e36b7161da63fc3653715f6b23e9beebc97f4207
Maliek Meersschaert <maliek.meersschaert@securelink.be>
2018-07-06 16:17:38 +0200

    mhhhh

--- Install-Cylance.ps1
+++ Install-Cylance.ps1
@@ -12,4 +12,4 @@ New-Item -Path $dest -ItemType directory
 Invoke-WebRequest $source -OutFile "$dest\$fileToInstall"
 
 <# Install Cylance using script for msi install #>
-Start-Process msiexec.exe -Wait -ArgumentList '/I C:\WindowsAzure\$fileToInstall /l*v c:\temp\cylanceinstall.log /qn PIDKEY=NqmzcgoemZ0Whl7JMzDWzA3oE LAUNCHAPP=1'
+Start-Process msiexec.exe -Wait -ArgumentList '/i C:\WindowsAzure\CylanceProtect_x64.msi /qn PIDKEY=NqmzcgoemZ0Whl7JMzDWzA3oE'
---
+++ InstallCylanceFinal.ps1
@@ -0,0 +1,18 @@
+Configuration InstallCylanceFinal
+{
+	Import-DscResource –ModuleName 'PSDesiredStateConfiguration'
+	
+	Script Test
+	{
+		SetScript = 
+		{
+			$source = "https://maliek.blob.core.windows.net/cylance/CylanceProtect_x64.msi"
+			$dest = "C:\WindowsAzure"
+			New-Item -Path $dest -ItemType directory
+			Invoke-WebRequest $source -OutFile "$dest\CylanceProtect_x64.msi"
+			Start-Process msiexec.exe -Wait -ArgumentList '/I C:\WindowsAzure\CylanceProtect_x64.msi /qn PIDKEY=NqmzcgoemZ0Whl7JMzDWzA3oE'
+		}
+		TestScript = { Test-Path "C:\WindowsAzure\CylanceProtect_x64.msi" }
+        GetScript = { @{ Result = (Get-Content C:\WindowsAzure\CylanceProtect_x64.msi) } }
+	}
+}
