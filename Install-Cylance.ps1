1185d5324e3139bc59f0e7bafa4fd012658cdd02
Maliek Meersschaert <maliek.meersschaert@securelink.be>
2018-07-06 11:26:25 +0200

    Fist steps

---
+++ Install-Cylance.ps1
@@ -0,0 +1,15 @@
+<# Custom Script for Windows to install Cylance from Azure Storage using the staging folder created by the deployment script #>
+param (
+    [string]$artifactsLocation,
+    [string]$artifactsLocationSasToken,
+    [string]$folderName,
+    [string]$fileToInstall
+)
+
+$source = $artifactsLocation + "\$folderName\$fileToInstall" + $artifactsLocationSasToken
+$dest = "C:\WindowsAzure\$folderName"
+New-Item -Path $dest -ItemType directory
+Invoke-WebRequest $source -OutFile "$dest\$fileToInstall"
+
+<# Install Cylance using script for msi install #>
+Start-Process msiexec.exe -Wait -ArgumentList '/I C:\WindowsAzure\$fileToInstall /l*v c:\temp\cylanceinstall.log /qn PIDKEY=INSERT_YOUR_KEY_HERE LAUNCHAPP=1'
