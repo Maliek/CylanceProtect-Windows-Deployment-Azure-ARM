fd8639161937d2a4f60a75a03a1e8a1d74d5ff68
Maliek <maliek.meersschaert@hotmail.com>
2018-07-11 13:53:23 +0200

    adding OPTICS installation

--- Install-Cylance.ps1
+++ Install-Cylance.ps1
@@ -3,13 +3,20 @@ param (
     [string]$artifactsLocation,
     [string]$artifactsLocationSasToken,
     [string]$folderName,
-    [string]$fileToInstall
+    [string]$fileToInstall,
+	[string]$fileToInstallTwo
 )
 
 $source = $artifactsLocation + "\$folderName\$fileToInstall" + $artifactsLocationSasToken
+$sourceTwo = $artifactsLocation + "\$folderName\$fileToInstallTwo" + $artifactsLocationSasToken
+
 $dest = "C:\WindowsAzure\$folderName"
 New-Item -Path $dest -ItemType directory
 Invoke-WebRequest $source -OutFile "$dest\$fileToInstall"
+Invoke-WebRequest $sourceTwo -OutFile "$dest\$fileToInstallTwo"
+
 
 <# Install Cylance using script for msi install #>
 Start-Process msiexec.exe -Wait -ArgumentList '/i C:\WindowsAzure\CylanceProtect_x64.msi /qn PIDKEY=XXXXXXXXXXXXXXXXXXXXXXXX LAUNCHAPP=1'
+
+Start-Process -FilePath "C:\WindowsAzure\CylanceOPTICSSetup.exe" -ArgumentList -q -Wait
